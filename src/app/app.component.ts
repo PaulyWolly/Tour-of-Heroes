fec7e567ed3c1ef19d1ceb5fae4eee636442af32
Paul Welby <pwelby@gmail.com>
2021-10-23 14:03:27 -0700

    Initial commit for Tour of Heroes application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-tour-of-heroes';
+  title = 'Tour of Heroes';
+  hero: string = 'Windstorm';
+
 }
